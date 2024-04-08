8e34690f9930221227759e5b043b4eda13a1d3ff
Diana Vladimirova <miss.vladimirova-diana@yandex.ru>
2024-04-08 14:42:01 +0300

    add file in package.json

--- src/games/gjg.js
+++ src/games/gjg.js
@@ -1,13 +1,13 @@
 const getAllDividers = (dividers) => {
-  let i = dividers[0] - 1
+  let i = dividers[0] - 1;
   while (i > 0) {
     if (dividers[0] % i === 0) {
-      dividers.push(i)
+      dividers.push(i);
     }
-    console.log(dividers.length)
-    i -= 1
+    console.log(dividers.length);
+    i -= 1;
   }
-  return dividers.length
-}
+  return dividers.length;
+};
 
-getAllDividers([16])
+getAllDividers([16]);
